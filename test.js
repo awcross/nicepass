66f8fd4ee2ce52354805af51178910e2301d4c56
Alex Cross <crossaw@gmail.com>
2020-02-05 22:40:29 -0800

    Bump dependencies and update tests

--- test.js
+++ test.js
@@ -1,17 +1,19 @@
-import test from 'ava';
-import execa from 'execa';
+const test = require('ava');
+const execa = require('execa');
 
 test('check length', async t => {
-	const {stdout} = await execa('./cli.js', [8]);
+	const {stdout} = await execa('./cli.js', [10]);
 
 	t.is(typeof stdout, 'string');
-	t.is(stdout.split(' ').length, 4);
+	t.is(stdout.split(' ').length, 5);
 });
 
 test('check hex', async t => {
-	t.is(await execa.stdout('./cli.js', ['1a5a5a74c970dda2']), 'byroad glorification showcase tacking');
+	const {stdout} = await execa('./cli.js', ['1a5a5a74c970dda2']);
+	t.is(stdout, 'byroad glorification showcase tacking');
 });
 
 test('check phrase', async t => {
-	t.is(await execa.stdout('./cli.js', ['faunae poorish petrographic woodenware']), '4cc5a575a003fd2b');
+	const {stdout} = await execa('./cli.js', ['faunae poorish petrographic woodenware']);
+	t.is(stdout, '4cc5a575a003fd2b');
 });
